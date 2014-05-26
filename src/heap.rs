c341540953f199f148cc60f231988d457f541b09
Jacob Mick <anon.biohacker@gmail.com>
2014-05-26 13:28:55 -0500

    cleaned up heap docs

--- src/heap.rs
+++ src/heap.rs
@@ -1,14 +1,23 @@
+// heap.rs
+
+//! Utility functions and algorithms for minimun heaps
+
+
+/// I implement this as a trait.
+///
+/// If I was only interested in heap_sort, I'd 
+/// write something like
+/// fn heap_sort<T: Ord>(&mut Vec<T>) {
+///      fn min_heapify<T: Ord>(&mut Vec<T>) { ...
+///      }
+///      ...
+/// }
+
 use std::fmt::{Show};
 use std::ptr::{swap};
 use std::vec::{Vec};
 
-//! If I was only interested in heap_sort, I'd 
-//! write something like
-//! fn heap_sort<T: Ord>(&mut Vec<T>) {
-//!      fn min_heapify<T: Ord>(&mut Vec<T>) { ...
-//!      }
-//!      ...
-//! }
+
 pub trait Heap {
     fn heap_sort(&mut self);
     fn min_heapify(&mut self, uint, uint);
